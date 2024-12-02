048d2ef7ff0624b4969001e3a3717e1ee916d151
biomorphs <matt.hoyle@gmail.com>
2024-12-02 20:00:12 +0000

    Day 1 part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -33,7 +33,6 @@ bool ParseLocations(const std::string& inputData, Locations& locs1, Locations& l
 	LocationID loc1, loc2;
 	while ((stream >> loc1) && (stream >> loc2))
 	{
-		std::cout << std::format("{} {}", loc1, loc2) << std::endl;
 		locs1.push_back(loc1);
 		locs2.push_back(loc2);
 	}
@@ -76,6 +75,23 @@ int main(int argc, char** args)
 	}
 
 	std::cout << std::format("Total distance = {}", totalDistance) << std::endl;
+
+	// Part 2, similarity scores!
+	int32_t totalSimilarity = 0;
+	for (const auto& loc1 : locations1)
+	{
+		auto foundInList2 = std::find(locations2.begin(), locations2.end(), loc1);
+		int32_t foundCount = 0;
+		while (foundInList2 != locations2.end() && *foundInList2 == loc1)
+		{
+			foundCount++;
+			foundInList2++;
+		}
+		const int32_t similarityScore = loc1 * foundCount;
+		totalSimilarity += similarityScore;
+	}
+
+	std::cout << std::format("Total Similarity = {}", totalSimilarity) << std::endl;
 
 	return 0;
 }

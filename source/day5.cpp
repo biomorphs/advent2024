5c8bb4cc84be5ce7363da84297f68e68e37b9a60
biomorphs <matt.hoyle@gmail.com>
2024-12-06 22:04:03 +0000

    Day 5 part 1 refactor for part 2

--- source/day5.cpp
+++ source/day5.cpp
@@ -40,6 +40,45 @@ void OutputUpdatePages(const UpdatePages& update)
 	std::cout << std::endl;
 }
 
+bool PageOrderCorrect(const SortRules& allRules, int32_t page1, int32_t page2)
+{
+	auto rulesFound = allRules.find(page1);	// are there rules for page1?
+	if (rulesFound != allRules.end())
+	{
+		auto& entries = rulesFound->second.m_entries;
+		auto foundEntry = std::find_if(entries.begin(), entries.end(), [&](const SortEntry& thisEntry) {
+			return thisEntry.m_value == page2;
+		});
+		if (foundEntry != entries.end())
+		{
+			if (foundEntry->m_type == SortType::Before)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool UpdateIsInOrderRefactor(const SortRules& allRules, const UpdatePages& update)
+{
+	for (int32_t index1 = 0; index1 < update.size(); ++index1)
+	{
+		for (int32_t index2 = 0; index2 < update.size(); ++index2)
+		{
+			if (index1 < index2 && !PageOrderCorrect(allRules, update[index1], update[index2]))
+			{
+				return false;
+			}
+			else if (index1 > index2 && !PageOrderCorrect(allRules, update[index2], update[index1]))
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 bool UpdateIsInOrder(const SortRules& allRules, const UpdatePages& update)
 {
 	for (int32_t index1 = 0; index1 < update.size(); ++index1)
@@ -142,7 +181,7 @@ int main(int argc, char** args)
 	for (int updateIndex = 0; updateIndex < allUpdates.size(); ++updateIndex)
 	{
 		OutputUpdatePages(allUpdates[updateIndex]);
-		bool matchesRules = UpdateIsInOrder(allRules, allUpdates[updateIndex]);
+		bool matchesRules = UpdateIsInOrderRefactor(allRules, allUpdates[updateIndex]);
 		std::cout << (matchesRules ? "\tMatch!" : "\tNo Match!") << std::endl;
 
 		// Find middle page
